8a7d7734f8bf8cd7874be2642b1f20073b4304ae
Wesllen Fernandes <wesllen20240028925@alu.uern.br>
2025-09-03 11:56:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.delete('/livros/:id', async (req, res) => {
 });
 
 // Inicia o servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
